992b2c845a329890b493a9fcb2f6c6ff52e3d11b
Filipe Souza Santos <silvertrident2@gmail.com>
2021-04-26 23:31:53 -0300

    Created aboutView

--- src/controllers/MainViewController.java
+++ src/controllers/MainViewController.java
@@ -1,11 +1,20 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import util.Alerts;
 
 public class MainViewController implements Initializable{
 	
@@ -31,12 +40,27 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("menuItemAbout");
+		loadView("/gui/AboutView.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle srcb) {
-		
+	}
+	
+	private synchronized void loadView(String srcView) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(srcView));
+			VBox newVBox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		} catch ( IOException e) {
+			Alerts.showAlert("IOException", "Internal error, source view not found", e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 
